837969a533f9547f773203ff075147967a02d226
GhostTal <1760074636@qq.com>
2021-11-24 14:55:39 +0800

    搭建网络基础框架-2

--- basicstructure/app/src/main/java/com/example/basicstructure/http/RetrofitFactory.kt
+++ basicstructure/app/src/main/java/com/example/basicstructure/http/RetrofitFactory.kt
@@ -1,10 +1,78 @@
 package com.example.basicstructure.http
 
+import com.example.baselib.BaseApp.Companion.getContext
+import com.example.baselib.http.HttpLoggingInterceptor
+import com.example.basicstructure.constants.ApiConstants
+import com.example.basicstructure.constants.Constants
+import com.franmontiel.persistentcookiejar.ClearableCookieJar
+import com.franmontiel.persistentcookiejar.PersistentCookieJar
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+import java.util.logging.Level
 
 object RetrofitFactory {
     //缓存100M
     private val okHttpClientBuilder: OkHttpClient.Builder
         get() {
+            return OkHttpClient.Builder()
+                .readTimeout(
+                    Constants.DEFAULT_TIMEOUT.toLong(),
+                    TimeUnit.MILLISECONDS
+                )
+                .connectTimeout(
+                    Constants.DEFAULT_TIMEOUT.toLong(),
+                    TimeUnit.MILLISECONDS
+                )
+                .addInterceptor(getLogInterceptor())
+                .cookieJar(getCookie())
+                .cache(getCache())
         }
+
+    fun factory(): Retrofit {
+        val okHttpClient = okHttpClientBuilder.build()
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(ApiConstants.BASE_URL)
+            .build()
+    }
+
+    /**
+     * 获取缓存方式
+     */
+
+    private fun getCache(): Cache? {
+        //缓存100M
+        return Cache(
+            File(getContext().cacheDir, "cache"),
+            1024 * 1024 * 1024
+        )
+    }
+
+    /**
+     * 获取cookie持久化
+     */
+    private fun getCookie(): ClearableCookieJar {
+        return PersistentCookieJar(
+            SetCookieCache(),
+            SharedPrefsCookiePersistor(getContext())
+        )
+    }
+
+
+    private fun getLogInterceptor(): Interceptor {
+
+        //http log拦截器
+        return HttpLoggingInterceptor("Okhttp").apply {
+            setPrintLevel(HttpLoggingInterceptor.Level.BODY)
+            setColorLevel(Level.INFO)
+        }
+    }
 }
